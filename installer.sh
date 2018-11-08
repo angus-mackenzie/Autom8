b739dcd476c942cc4e7369428698972515350cbf
Angus Mackenzie <angusmackenzie111@gmail.com>
2018-11-08 11:16:36 +0200

    Added test script

---
+++ installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update && 
+sudo apt-get upgrade && 
+sudo apt-get install g++ && 
+sudo apt-get install -y git && 
+sudo apt-get install -y build-essentials && 
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && 
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ && 
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' && 
+sudo apt-get install apt-transport-https &&
+sudo apt-get update &&
+sudo apt-get install code

d5fc090051533fcbf0c93474882fcee527fb56b1
Angus Mackenzie <angusmackenzie111@gmail.com>
2018-11-25 15:28:17 +0200

    Changed order of installations

--- Vagrant/installer.sh
+++ Vagrant/installer.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y g++
+sudo apt-get install -y git
 git config --global user.email "myemail@gmail.com"
 git config --global user.name "Angus"
-sudo apt-get install -y git
 sudo apt-get install -y build-essential
 sudo apt install -y default-jdk
